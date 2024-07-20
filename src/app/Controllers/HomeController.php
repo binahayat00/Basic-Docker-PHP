81337c7cdfa2964250d3128350cc94a1f827b156
amir <amirjoun2@gmail.com>
2024-07-20 19:08:39 +0400

    Added simple 404 page, uploading file

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -12,4 +12,13 @@ class HomeController
     {
         return View::make("index",['title' => 'Home Page']);
     }
+
+    public function upload()
+    {
+        $filePath = STORAGE_PATH .'/' . $_FILES['receipt']['name'];
+
+        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
+        header('Location: /');
+        exit;
+    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,18 +1,26 @@
 <?php
 
+use App\View;
 use App\Router;
+use App\Exception\RouteNotFoundException;
 
 require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 define('STORAGE_PATH',__DIR__. '/../storage');
 define('VIEW_PATH',__DIR__. '/../views');
-
+try {
 $router = new Router();
 
 $router->get('/',[App\Controllers\HomeController::class, 'index']);
+$router->post('/upload',[App\Controllers\HomeController::class, 'upload']);
 $router->get('/invoices',[App\Controllers\InvoicesController::class, 'index']);
 $router->get('/invoices/create',[App\Controllers\InvoicesController::class, 'create']);
 $router->post('/invoices/create',[App\Controllers\InvoicesController::class, 'store']);
 
 echo $router->resolve($_SERVER['REQUEST_URI'],$_SERVER['REQUEST_METHOD']);
+} catch (RouteNotFoundException $e) {
+    // header('HTTP/1.1 404 Not Found');
+    http_response_code(404);
+    echo View::make('errors/404');
+}
